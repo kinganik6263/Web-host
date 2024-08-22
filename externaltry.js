f0f5fda523d09b1d20d667c47dd61ec6545d888f
kinganik6263 <161469632+kinganik6263@users.noreply.github.com>
2024-08-22 12:13:39 +0800

    Create externaltry.js

---
+++ externaltry.js
@@ -0,0 +1,21 @@
+function lt() {
+ document.getElementById('lt').src = 'on.gif'
+ console.log('lite off')  
+}
+
+function of() {
+ document.getElementById('lt').src = 'off.gif' 
+ 
+ console.log('lite on') 
+}
+
+function boff() {
+document.getElementById('box').innerText = ('off')
+    
+}
+
+
+function bon() {
+document.getElementById('box').innerText = ('on')
+    
+}
